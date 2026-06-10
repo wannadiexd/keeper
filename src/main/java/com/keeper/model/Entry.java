37d23ccfcd63b6042cbf8c2c73bf86691528d911
Arslan_Diveyev <Arslan_Diveyev@student.itpu.uz>
2026-06-11 02:19:15 +0300

    add entry model

---
+++ src/main/java/com/keeper/model/Entry.java
@@ -0,0 +1,38 @@
+package com.keeper.model;
+
+public class Entry {
+    private int id;
+    private String title;
+    private String username;
+    private String password;
+    private String url;
+    private String category;
+
+    public Entry() {}
+
+    public Entry(String title, String username, String password, String url, String category) {
+        this.title = title;
+        this.username = username;
+        this.password = password;
+        this.url = url;
+        this.category = category;
+    }
+
+    public int getId() { return id; }
+    public void setId(int id) { this.id = id; }
+
+    public String getTitle() { return title; }
+    public void setTitle(String title) { this.title = title; }
+
+    public String getUsername() { return username; }
+    public void setUsername(String username) { this.username = username; }
+
+    public String getPassword() { return password; }
+    public void setPassword(String password) { this.password = password; }
+
+    public String getUrl() { return url; }
+    public void setUrl(String url) { this.url = url; }
+
+    public String getCategory() { return category; }
+    public void setCategory(String category) { this.category = category; }
+}
